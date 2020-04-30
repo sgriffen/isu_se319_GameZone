0f297caa58677b38c13cb22c805bb910302a19bc
Rob <rrbarton@iastate.edu>
2020-04-30 15:42:44 -0500

    Fixed a syntax error

--- gameZone/src/test/resources/frontendLogic.test.js
+++ gameZone/src/test/resources/frontendLogic.test.js
@@ -26,7 +26,7 @@ const { setmyTurn,setPlayer,sendBackend,setBackend,updateCell,requestAI,requestH
 	= require('./../../main/resources/static/js/frontendLogic.js')
 
 setStorage(new LocalStorageMock);
-var back=jest.fn(();
+var back=jest.fn();
 var x="<img src='images/x.png' style='width:95%;height:95%;'>";
 var o="<img src='images/o.png' style='width:95%;height:95%;'>";
 setBackend(back);
