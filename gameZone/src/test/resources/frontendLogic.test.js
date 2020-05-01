4da3ee52d7d38e5c1d4013ed7b11330c75d6c1d9
Rob <rrbarton@iastate.edu>
2020-05-01 04:11:15 -0500

    updated 2/3 frontend tests

--- gameZone/src/test/resources/frontendLogic.test.js
+++ gameZone/src/test/resources/frontendLogic.test.js
@@ -32,15 +32,6 @@ var x="<img src='images/x.png' style='width:95%;height:95%;'>";
 var o="<img src='images/o.png' style='width:95%;height:95%;'>";
 setBackend(back);
 
-test('test init', () => {
-	init(null);
-	expect(sto.getItem("userID")).not.toBeNull()
-	expect(getSocket).toBeDefined();
-	expect(getSocket).not.toBeNull();
-	closeSocket();
-});
-
-
 test('test updating cell', () => {
 	let cell="<div></div>";
 	setPlayer(true);
@@ -57,7 +48,7 @@ test('test request AI', () => {
 	expect(back).toBeDefined();
 	requestAI()
 	expect(back.mock.calls[0][0]).toBe(202);
-	expect(back.mock.calls[0][1]).toBe("AI");
+	expect(back.mock.calls[0][1]).toBe(["AI"]);
 	expect(back.mock.calls[0][2]).toBe(0);
 	expect(back.mock.calls[0][3]).toBe(12345);
 });
@@ -65,7 +56,7 @@ test('test request AI', () => {
 test('test request human', () => {
 	requestHuman(2020)
 	expect(back.mock.calls[1][0]).toBe(202);
-	expect(back.mock.calls[1][1]).toBe(2020);
+	expect(back.mock.calls[1][1]).toBe([2020]);
 	expect(back.mock.calls[1][2]).toBe(0);
 	expect(back.mock.calls[1][3]).toBe(12345);
 });
